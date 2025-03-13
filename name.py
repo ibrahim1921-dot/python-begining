56e77de6a4b820e1b946d3e55f917ef472d23add
[Abdul-Sobur Ibrahim] <[abdulsobur@gmail.com]>
2025-03-13 13:39:58 +0000

    first python push

---
+++ name.py
@@ -0,0 +1,13 @@
+#this program asks for the users name and password
+while True:
+    print('Enter your name!')
+    name=input()
+    if name=='Joe':
+        continue
+
+    print('What is the password?')
+    password=input()
+    if password!='abdulsobur':
+        break
+print('Welcome to your portal, We wish you a happy browsing.')
+
---
+++ sobur.py
@@ -0,0 +1,16 @@
+'''name='your name'
+while name:
+    print('Enter your name')
+    name=input()
+
+print('Thank you')'''
+
+while True:
+    print('Enter your name')
+    name=input()
+    if name=='Sobur':
+        continue
+print('Thank you')
+
+
+
