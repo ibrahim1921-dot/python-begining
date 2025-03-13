9c5753569b7444d200ceb2d438ba82dfe092e8e0
[Abdul-Sobur Ibrahim] <[abdulsobur@gmail.com]>
2025-03-13 13:46:01 +0000

    name

--- sobur.py
+++ sobur.py
@@ -1,15 +1,17 @@
-'''name='your name'
+#giving a while loop a try
+name='your name'
 while name:
     print('Enter your name')
     name=input()
 
-print('Thank you')'''
+print('Thank you')
+#trying out break statement
 
 while True:
     print('Enter your name')
     name=input()
     if name=='Sobur':
-        continue
+        break
 print('Thank you')
 
 
